f36f2b6ee81284d34ba54b37574f0362d978804b
Kasuletrevor <kasulejohntrevor@gmail.com>
2026-02-03 23:28:30 +0300

    Update CORS configuration to support all routes and streamline allowed origins

--- appv2.py
+++ appv2.py
@@ -1,6 +1,5 @@
 """Flask app to remove PII from x-ray images."""
 
-
 from flask import Flask, Response, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
 from ultralytics import YOLO
@@ -13,17 +12,21 @@ import math
 from inference.inference_yolo_models import yolo_predict
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5173",
-                                                    "http://localhost:4173",
-                                                    "http://34.31.76.1:8080",
-                                                    "https://dsi.emergentai.ug"]}},
-                                                    supports_credentials=True,
-                                                    methods=["*"],
-                                                    allow_headers=["*"],
-                                                )
+cors = CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": ["http://localhost:5173", "https://dsi.emergentai.ug"]
+        }
+    },
+    supports_credentials=True,
+    methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load the model once the app starts
 model = YOLO("phi_models/best.pt")
+
 
 @app.route("/predict", methods=["POST"])
 @cross_origin()
@@ -40,15 +43,17 @@ def web_clean_image() -> Response | tuple[Response, int]:
     file = request.files["image"]
 
     bboxes = yolo_predict(model, [file.stream], device="cpu")
-    all_bboxes = [([bbox["xmin"], bbox["ymin"], bbox["xmax"], bbox["ymax"]])
-                   for bbox in bboxes]
+    all_bboxes = [
+        ([bbox["xmin"], bbox["ymin"], bbox["xmax"], bbox["ymax"]])
+        for bbox in bboxes
+    ]
     all_bboxes = [[math.ceil(x) for x in bbox] for bbox in all_bboxes]
 
     image = Image.open(file)
     image_array = np.array(image)
     for bbox in all_bboxes:
-        image_array[bbox[1]:bbox[3], bbox[0]:bbox[2]] = 0
-    
+        image_array[bbox[1] : bbox[3], bbox[0] : bbox[2]] = 0
+
     # Convert numpy array back to image
     clean_image = Image.fromarray(image_array)
 
